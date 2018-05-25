58225a421e87ae8f09b7b3e7f211fca22d12cd4f
Feng Chen <fchen14@lsu.edu>
2018-05-24 23:41:24 -0500

    adding instruction for exercise1

--- day1morning/exercise/run_lp_mpi.sh
+++ day1morning/exercise/run_lp_mpi.sh
@@ -5,5 +5,5 @@ mpicc lp_mpi.c -o lp_mpi.out
 # 10000 timesteps, print information every 100 steps, do not print 
 # matrix 0
 mpirun -np 16 ./lp_mpi.out 4096 4096 4 4 0.0001 10000 100 0
-# open another terminal and go to the compute node, then type “top”
+# open another terminal and go to the compute node (ssh shelobxxx), then type “top”
 
